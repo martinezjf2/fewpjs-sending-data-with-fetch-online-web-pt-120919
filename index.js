979acbf1cfe406ffd9ae181a7c051321f12ea282
Jeffrey Martinez <martinezjf2@gmail.com>
2022-03-02 13:26:11 -0500

    commit tree 9b9948b6d4a06da37c9912381cdff1f11ea84c61

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+
+  function submitData(name, email){
+    let formData = {
+        name: name,
+        email: email
+      };
+      
+      // method: "POST" is missing from the object below
+      let configObj = {
+          method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+        body: JSON.stringify(formData)
+      };
+
+    fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      document.body.textContent = object.id;
+    })
+    .catch(function(error) {
+      alert("Bad things! Ragnarők!");
+      console.log(error.message);
+    });
+
+  }
+  
